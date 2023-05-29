c94370ee9e8ff0b3109d3670a9b8d6e03253e8d5
Roberto Zalazar <servicios773@gmail.com>
2023-05-29 12:07:19 -0300

    cambios

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+const sumar = function (a, b) {
+    return a + b;
+}
+const restar = function (a, b) {
+    return a - b;
+}
+
+const multiplicar = function (a, b) {
+    return a * b;
+}
+
+
+const dividir = function (a, b) {
+    return a / b;
+}
+
+module.exports={
+    sumar,
+    restar,
+    multiplicar,
+    dividir
+}
